c065c84fac2a7036f156e06e48e367ced4b10e19
Chris Neal <chrisvneal@gmail.com>
2020-12-15 19:58:54 -1000

    Change the name of links

--- src/Components/Links.js
+++ src/Components/Links.js
@@ -4,8 +4,7 @@ import { Link } from "react-router-dom";
 function Links() {
   return (
     <div className="links">
-      <h3>Links!</h3>
-      <Link to="/">Home</Link>
+      <Link to="/">New Tasks</Link>
 
       <Link to="/oldtasks">OldTasks</Link>
     </div>
