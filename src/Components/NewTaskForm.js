5df1fd11c64c6ca349cbb6d2ee4cecd69d018ba7
Chris Neal <chrisvneal@gmail.com>
2020-12-15 21:22:43 -1000

    Add form and input to homepage

---
+++ src/Components/NewTaskForm.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function NewTaskForm() {
+  return (
+    <form className="container">
+      <p>Enter new task</p>
+      <input type="text" placeholder="wash dishes" />
+    </form>
+  );
+}
+
+export default NewTaskForm;
