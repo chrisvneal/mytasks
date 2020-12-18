13422c1107481bc234d4a35e83301be96aa3b311
Chris Neal <chrisvneal@gmail.com>
2020-12-17 14:27:34 -1000

    Remove add task form from new page

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,14 +1,11 @@
 import React from "react";
 import TaskList from "./TaskList";
 import Header from "./Header";
-import NewTaskForm from "./NewTaskForm";
 
 function Home(props) {
   return (
     <div className="Home">
       <Header />
-
-      <NewTaskForm />
 
       <div className="container">
         <h2>New Tasks</h2>
